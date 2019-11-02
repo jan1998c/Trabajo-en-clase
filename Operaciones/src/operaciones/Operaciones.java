784bb64279f516c118dedbdc5df3af3d23dc7ca8
jan98 <jcgonzalez507@misena.edu.co>
2019-11-01 19:05:44 -0500

    Version 2

--- Operaciones/src/operaciones/Operaciones.java
+++ Operaciones/src/operaciones/Operaciones.java
@@ -1,12 +1,15 @@
 
 package operaciones;
 
+import java.util.Scanner;
+
 
 public class Operaciones {
 
     
     public static void main(String[] args) {
-        
+        Scanner opc = new Scanner(System.in);
+        System.out.println("¿Digite Su Edad?");
     }
     
 }
