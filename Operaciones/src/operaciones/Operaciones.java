0b10f9c1906fe590cf34b4fafbb1a76cd756b43d
jan98 <jcgonzalez507@misena.edu.co>
2019-11-01 19:36:58 -0500

    ejemplo

--- Operaciones/src/operaciones/Operaciones.java
+++ Operaciones/src/operaciones/Operaciones.java
@@ -10,6 +10,7 @@ public class Operaciones {
     public static void main(String[] args) {
         Scanner opc = new Scanner(System.in);
         System.out.println("¿Digite Su Edad?");
+        //
     }
     
 }
